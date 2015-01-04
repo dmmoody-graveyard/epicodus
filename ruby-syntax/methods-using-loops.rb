03e2d67b6d906c4f2ea0398b997e80c5063cba83
Duane M Moody <dmmoody@gmail.com>
2015-01-04 14:44:45 -0800

    methods using loops lesson

---
+++ ruby-syntax/methods-using-loops.rb
@@ -0,0 +1,36 @@
+class Array
+  define_method(:upcase) do
+    new_array = []
+    self.each() do |string|
+      upcased_string = string.upcase()
+      new_array.push(upcased_string)
+    end
+    new_array
+  end
+end
+
+["a", "b", "c"].upcase()
+
+class Range
+  define_method(:sum) do
+    total = 0
+    self.each() do |number|
+      total = total.+(number)
+    end
+    total
+  end
+end
+
+(1..5).sum()
+
+class Array
+  define_method(:upcase!) do
+    self.each() do |string|
+      string.upcase!()
+    end
+  end
+end
+
+my_array = ["a", "b", "c"]
+my_array.upcase!()
+my_array
