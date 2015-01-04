b65452a847374cd9d50e29b9179db71d52610031
Duane M Moody <dmmoody@gmail.com>
2015-01-03 20:36:08 -0800

    Receivers vs return values lesson

---
+++ ruby-syntax/receivers-versus-return-values.rb
@@ -0,0 +1,44 @@
+numbers = [1, 2, 3]
+numbers.push(4)
+numbers
+food = "broccoli"
+food.upcase()
+food
+food = "broccoli"
+food.upcase!()
+food
+my_number = 5
+my_number.next()
+my_number
+a = 4
+a.+(10)
+a
+a.-(3)
+a
+a.*(9)
+a
+a./(3)
+a
+a.**(2)
+a
+a = "Hello World"
+a.split(" ")
+a
+a = "Hello World"
+a.split!(" ")
+a
+a = ["Hello", "World"]
+a.join(" ")
+a
+a = ["Hello", "World"]
+a.join!(" ")
+a
+a = "hello world"
+a.capitalize()
+a
+a = "hello world"
+a.capitalize!()
+a
+a.reverse()
+a
+a.reverse!()
