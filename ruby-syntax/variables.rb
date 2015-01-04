2d499662af95d7856a7155e86f65300b3b98f430
Duane M Moody <dmmoody@gmail.com>
2015-01-03 19:52:21 -0800

    Variables lesson

---
+++ ruby-syntax/variables.rb
@@ -0,0 +1,15 @@
+name = "michael"
+name.length()
+name = "christopher"
+name
+name.length()
+favorite_number = 13
+favorite_number
+favorite_number.+(1)
+favorite_number = 8
+favorite_number
+favorite_number.+(1)
+age_of_bob = 20
+my_age = 39
+both_ages = age_of_bob.+(my_age)
+new_age_of_bob = age_of_bob.+(1)
